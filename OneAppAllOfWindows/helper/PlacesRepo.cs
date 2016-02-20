3b57a85891ed3104c30602cae575e769e0122422
AMRALAA10 <amr.alaa20191404@gmail.com>
2016-02-20 19:40:37 +0200

    global and local styles

---
+++ OneAppAllOfWindows/helper/PlacesRepo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OneAppAllOfWindows.model;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using Windows.Storage;
+
+namespace OneAppAllOfWindows.helper
+{
+    public static class PlacesRepo
+    {
+        private static List<Place> allPlaces;
+
+        public async static Task<List<Place>> GetData(StorageFile file)
+        {
+            if (allPlaces != null)
+                return allPlaces;
+
+
+
+            string jsonText = await FileIO.ReadTextAsync(file);
+
+
+            //StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appdata:///places.json"));
+            var fileStream = await file.OpenStreamForReadAsync();
+    
+
+            var serializer = new DataContractJsonSerializer(typeof(List<Place>));
+            allPlaces = (List<Place>)serializer.ReadObject(fileStream);
+
+            return allPlaces;
+
+        }
+    }
+}
---
+++ OneAppAllOfWindows/model/Place.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneAppAllOfWindows.model
+{
+    public class Place
+    {
+        public string UniqueId { get; set; }
+        public string Title { get; set; }
+        public string building { get; set; }
+        public int Floor { get; set; }
+        public string ImagePath { get; set; }
+        public string Description { get; set; }
+
+    }
+}
